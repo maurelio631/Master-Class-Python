9c9190ec2b730831e6ef38c76b0aba78b450b942
Marco <51382551+maurelio631@users.noreply.github.com>
2024-04-01 14:27:06 +0000

    Trabalhando com condicionais

---
+++ src/controle/condicionais-1.py
@@ -0,0 +1,14 @@
+a = 4
+
+if a>5:
+    print("O valor é MAIOR do que 5")
+    print("Expressão gerou um resultado: True")
+
+    print("Continua dentro do bloco IF")
+else:
+    print("O valor é MENOR do que 5")
+    print("Expressão gerou um resultado: FALSE")
+
+    print("Continua dentro do bloco ELSE")
+
+print("Fim do código")
