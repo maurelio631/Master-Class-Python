ff26a5e3443b5f13697a8b8a416ddc6ad9450e94
Marco <51382551+maurelio631@users.noreply.github.com>
2024-04-01 17:42:32 +0000

    Brincando com o random usando a biblioteca cowsay

---
+++ src/funcao/funcao-3.py
@@ -0,0 +1,15 @@
+import cowsay
+import random
+
+def console_divertido(texto):
+    funcoes = [
+        cowsay.cow,
+        cowsay.dragon,
+        cowsay.turtle,
+        cowsay.tux,
+    ]
+
+    funcao = random.choice(funcoes)
+    funcao(texto)
+
+console_divertido("Olá python!")
