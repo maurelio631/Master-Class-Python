263c166797037705c5ee6f9ac562edafe800f6e6
Marco <51382551+maurelio631@users.noreply.github.com>
2024-04-01 14:19:17 +0000

    Aprendendo a criar e manipular um dicionário em python

---
+++ src/dados/dicionario.py
@@ -0,0 +1,11 @@
+aluno = {} #Criação de um novo dicionário
+
+aluno['nome']='João' # Chave Valor
+aluno['idade'] = 20
+aluno['curso']= 'Engenharia'
+
+print(aluno)
+
+del aluno['idade']
+
+print(aluno)
