2a7902467fa2b4c277d9297eb55e53c30547d461
Marco <51382551+maurelio631@users.noreply.github.com>
2024-04-01 13:18:52 +0000

    Aprendendo sobre a criação de variáveis, operadores matemáticos e tipos de comentários possíveis

---
+++ src/fundamentos/variaveis.py
@@ -0,0 +1,21 @@
+# print(3+7)
+
+# Em python não é necessário o uso de uma palavra reservada que indique a criação de uma variável
+a = 10
+b = 6
+
+# Operações matemáticas
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #Divisão que não retorna casas decimais, somente o primeiro número antes da vírgula
+print(a%b) #Módulo
+print(a**b) #Potência
+
+"""
+Comentário em bloco no python
+"""
+
+texto = "Bom dia!"
+print(texto*3) #Vai imprimir na tela "Bom dia!" 3 vezes
